309475f5a0c4b4032b67f304e38686bdb038ee34
Lucia <lumiraelmar@gmail.com>
2020-07-07 01:26:11 +0200

    practica react

--- desafio3-react/src/App.js
+++ desafio3-react/src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import InputWrapper from './components/InputWrapper';
-import Button from './components/Button';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue1: 0,
+      inputValue2: 0,
+      result: 0
+    }
+  }
+
+handleChange(e) {
+  const { name, value } = e.target
+  this.setState({
+    [name]: value
+  })
+}
+
+handleClick() {
+  const { firstNum, secondNum } = this.state
+  this.setState({
+    result: parseInt(firstNum) + parseInt(secondNum)
+  })
+}
+
   render() {
-    
+    const { result } = this.state
     return (
-      <div className='wrapper'>
-        <InputWrapper />
-        <Button button='Add'/>
-      </div>
+      <React.Fragment>
+        <input name='firstNum' onChange={(e) => this.handleChange(e)} type='text' placeholder='Add number'/>
+        <input name='secondNum' onChange={(e) => this.handleChange(e)} type='text' placeholder='Add number'/>
+        <button onClick={() => this.handleClick()}>Add</button>
+        <p>Resultado: {result}</p>
+      </React.Fragment>
     );
   }
 }
