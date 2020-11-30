7b9940d5bb705df7bed41ea48d5e08ab3ccb8271
Lucia <lumiraelmar@gmail.com>
2020-11-30 19:37:38 +0100

    all done

--- backend/ejercicios/index.js
+++ backend/ejercicios/index.js
@@ -1,93 +1,116 @@
-//1
-// function dayOfTheWeek(num) {
-//   const dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
-//   if (dias[num - 1]) {
-//     console.log(`El dia ${num} es ${dias[num - 1]}`);
-//   } else {
-//     console.log("tu numero no corresponde a un dia");
-//   }
-// }
-// dayOfTheWeek(10);
-
-//2
-// function month(str) {
-//   if (
-//     str == "enero" ||
-//     str == "marzo" ||
-//     str == "mayo" ||
-//     str == "julio" ||
-//     str == "agosto" ||
-//     str == "octubre" ||
-//     str == "diciembre"
-//   ) {
-//     console.log("el mes tiene 31 dias");
-//   } else if (str == "abril" || str == "junio" || str == "septiembre" || str == "noviembre") {
-//     console.log("el mes tiene 30 dias");
-//   } else if (str == "febrero") {
-//     console.log("el mes estupido de 28 o 29 dias");
-//   } else {
-//     console.log("hiciste un typo");
-//   }
-// }
-// month("abril");
-
-// 3
-// function number(num) {
-//   if (num < 0) {
-//     console.log("NEGATIVO");
-//   } else if (num == 0) {
-//     console.log("0");
-//   } else if (num > 0) {
-//     console.log("POSITIVO");
-//   }
-// }
-// number(0);
-
-//4
-
-//5
-
-//6
-// for (let i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
-
-//7
-// for (let i = 5; i >= 0; i--) {
-//   console.log(i);
-// }
-
-//8
-//
-
-//9
-// let x = 1;
-// while (x <= 10) {
-//   console.log(x);
-//   x++;
-// }
-
-//9.1
-// for (let i = 0; i <= 50; i++) {
-//   if (i % 2 == 0) {
-//     console.log(i);
-//   }
-// }
-
-//9.2
-
-// function vowelCounter(arr) {
-//   let counter = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] == "a" || arr[i] == "e" || arr[i] == "i" || arr[i] == "o" || arr[i] == "u") {
-//       counter++;
+// 1. Fizzbuzz
+// function fizzbuzz(num) {
+//   const arr = [];
+//   for (let i = 0; i <= num; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//       arr.push(`${i} - Fizzbuzz`);
+//     } else if (i % 3 == 0) {
+//       arr.push(`${i} - Fizz`);
+//     } else if (i % 5 == 0 && i % 5 == 0) {
+//       arr.push(`${i} - Buzz`);
+//     } else {
+//       arr.push(i);
 //     }
 //   }
-//   return counter;
+//   return arr;
 // }
 
-// const firstArr = vowelCounter(["a", "b", "c", "d", "e"]);
-// console.log(firstArr);
+// console.log(fizzbuzz(15));
 
-// const secondArr = vowelCounter(["c", "o", "u", "r", "s", "e", "i", "t"]);
-// console.log(secondArr);
+// 2. Mayor numero
+// function mayor(arr) {
+//   arr.sort((a, b) => a - b);
+//   return arr[arr.length - 1];
+// }
+
+// console.log(mayor([1, 6, 4, 9, 2]));
+
+// 3. Mayor 2 numeros
+// function mayor(arr) {
+//   arr.sort((a, b) => a - b);
+//   const mayores = [arr[arr.length - 2], arr[arr.length - 1]];
+//   return mayores;
+// }
+
+// console.log(mayor([1, 6, 4, 9, 2]));
+
+// 4. duplicados
+// function duplicados(arr) {
+//   let duplicados = [];
+//   const sorted = arr.sort((a, b) => a - b);
+
+//   for (let i = 0; i < sorted.length; i++) {
+//     if (sorted[i] == sorted[i + 1]) {
+//       duplicados.push(sorted[i]);
+//     }
+//   }
+//   const newSet = [...new Set(duplicados)];
+//   return newSet;
+// }
+
+// console.log(duplicados([1, 2, 3, 2, 3, 4, 1]));
+
+// 5. unicos
+// function unicos(arr) {
+//   let unicos = [];
+
+//   arr.forEach((num1) => {
+//     let count = 0;
+//     arr.forEach((num2) => {
+//       if (num1 == num2) {
+//         count++;
+//       }
+//     });
+//     if (count == 1) {
+//       unicos.push(num1);
+//     }
+//   });
+//   return unicos;
+// }
+
+// console.log(unicos([1, 2, 2, 3, 4, 1]));
+
+// 6. PALINDROMOS
+// function palindromos(str) {
+//   const reverse = str.split("").reverse().join("");
+//   if (reverse == str) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(palindromos("juani"));
+// console.log(palindromos("sugus"));
+
+// 7. INVERSO
+// function reverse(str) {
+//   const reverse = str.split("").reverse().join("");
+//   return reverse;
+// }
+
+// console.log(reverse("juani"));
+// console.log(reverse("sugus"));
+
+//DUPLICADOS NICO
+// function Repeated(arr) {
+//   const duplicates = [
+//     ...new Set(arr.filter((value, index, self) => self.indexOf(value) !== index)),
+//   ];
+
+//   return duplicates;
+// }
+// console.log(Repeated([1, 2, 2, 3, 4, 1]));
+
+//UNICOS
+function unique(arr) {
+  const duplicates = [
+    ...new Set(arr.filter((value, index, self) => self.indexOf(value) !== index)),
+  ];
+
+  const unique = arr.filter((num) => !duplicates.includes(num));
+
+  return unique;
+}
+
+console.log(unique([1, 2, 2, 3, 4, 1]));
