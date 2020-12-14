37ac3ef23a07b0b82e796e84bf44cc6f900a6b77
Lucia <lumiraelmar@gmail.com>
2020-12-14 17:55:16 +0100

    cambie palabras

--- backend/postman-project/controllers/UserController.js
+++ backend/postman-project/controllers/UserController.js
@@ -11,9 +11,11 @@ class UserController {
 
   postUser(req, res) {
     const { body, headers } = req;
-    if (body.name && headers.token) {
+    if (body.name && headers.token == "r2d2") {
       this.names.push(body.name);
       res.status(200).send("usuario creado correctamente");
+    } else if (headers.token != "r2d2") {
+      res.status(401).send("falta token");
     } else {
       res.status(400).send("falta name");
     }
@@ -22,23 +24,22 @@ class UserController {
   modifyUser(req, res) {
     const { body, headers } = req;
     const { name, i } = body;
-    console.log(headers);
-    if (name && i && headers.token) {
+    if (name && i && headers.token == "r2d2") {
       this.names.splice(i, 1, name);
-      res.status(200).send("usuario creado correctamente");
-    } else if (headers.token == null) {
+      res.status(200).send("usuario modificado correctamente");
+    } else if (headers.token != "r2d2") {
       res.status(401).send("falta token");
     } else {
-      res.status(400).send("no hay nada para borrar");
+      res.status(400).send("no hay nada para modificar");
     }
   }
 
   deleteUser(req, res) {
     const { body, headers, params } = req;
-    if (body.name && params.index && headers.token) {
+    if (body.name && params.index && headers.token == "r2d2") {
       this.names.splice(params.index, 1);
-      res.status(200).send("usuario creado correctamente");
-    } else if (headers.token == null) {
+      res.status(200).send("usuario eliminado correctamente");
+    } else if (headers.token != "r2d2") {
       res.status(401).send("falta token");
     } else {
       res.status(400).send("no hay nada para borrar");
